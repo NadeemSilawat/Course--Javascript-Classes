78a6923bfb30e731e36109c85e82ce7e92872eba
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-02-15 17:33:39 +0530

    Singletons JS - Design Pattern

---
+++ 06-Creating-a-html-Binding/listBinding.js
@@ -0,0 +1,47 @@
+
+class ListBinding {
+    constructor(element) {
+        this.listelement = element;
+        this.textList = [ ]
+
+
+    }
+    /**Make an <li> text </li> element/tag */
+    static createListItem(text) {
+        const li = document.createElement('li')
+
+        li.textContent = text
+
+        return li
+    }
+
+    update() {
+
+        /**Remove all existing <li> element/tages */
+
+        while (this.listelement.firstChild) {
+            this.listelement.removeChild(this.listelement.firstChild)
+        }
+
+        /**Fill <ul><ol> tag with <li>*/
+        for (const text of this.textList) {
+            this.listelement.appendChild(ListBinding.createListItem(text))
+
+        }
+    }
+
+    add(text){
+        this.textList.push(text);
+        this.update();
+    } 
+
+    remove(index){
+        thid.textList.splice(index,1);
+        this.update();
+
+    }
+    
+    
+
+
+}
